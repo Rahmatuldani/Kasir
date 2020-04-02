9be6caf25c9618b5e101ab77becbcf9d21b1049c
Rahmatuldani <danirahmatul@gmail.com>
2020-04-03 05:25:14 +0700

    kasir

--- src/Utama/vUtama.java
+++ src/Utama/vUtama.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableColumnModel;
 import java.awt.*;
+import java.text.*;
+import java.util.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class vUtama extends JFrame{
     JTabbedPane tab = new JTabbedPane();
@@ -13,15 +17,15 @@ public class vUtama extends JFrame{
 
 //    Bagian Kasir
     JLabel lno = new JLabel("No Transaksi : ",SwingConstants.RIGHT);
+    JLabel fno = new JLabel();
     JLabel ltanggal = new JLabel("Tanggal : ",SwingConstants.RIGHT);
+    JLabel ftanggal = new JLabel();
     JLabel lkasir = new JLabel("Kasir : ",SwingConstants.RIGHT);
+    JLabel fkasir = new JLabel();
     JLabel lmember = new JLabel("Member : ",SwingConstants.RIGHT);
     JLabel lkode = new JLabel("Kode Item : ",SwingConstants.RIGHT);
     JLabel ljum = new JLabel("Jumlah : ",SwingConstants.RIGHT);
 
-    JTextField fno = new JTextField(30);
-    JTextField ftanggal = new JTextField(30);
-    JTextField fkasir = new JTextField(30);
     String[] isi = {"UMUM","MEMBER"};
     JComboBox fmember = new JComboBox(isi);
     JTextField fkode = new JTextField(15);
@@ -34,10 +38,10 @@ public class vUtama extends JFrame{
 
     JPanel data = new JPanel();
     JPanel dbarang = new JPanel();
-    String[] column = {"No","Kode Item","Nama Barang","Harga","Jumlah","Total"};
+    String[] column = {"","No","Kode Item","Nama Barang","Harga","Jumlah","Total"};
     int a = 12000;
     int b = 20;
-    Object[][] tdata = {{"1","123","pensil",a,b,new Integer(a*b)}};
+    Object[][] tdata = {{"","1","123","pensil",a,b,new Integer(a*b)}};
     JTable table = new JTable(tdata,column);
     DefaultTableCellRenderer righttable = new DefaultTableCellRenderer();
     JScrollPane jScrollPane = new JScrollPane(table);
@@ -47,6 +51,7 @@ public class vUtama extends JFrame{
         setTitle("Sistem Kasir");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setExtendedState(MAXIMIZED_BOTH);
+//        setUndecorated(true);
         setLocationRelativeTo(null);
         setLayout(null);
         setVisible(true);
@@ -70,21 +75,29 @@ public class vUtama extends JFrame{
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         table.setRowHeight(20);
         table.setFont(new Font("SansSerif",Font.PLAIN,14));
-        table.getColumnModel().getColumn(0).setPreferredWidth(30);
-        table.getColumnModel().getColumn(1).setPreferredWidth(200);
-        table.getColumnModel().getColumn(2).setPreferredWidth(567);
-        table.getColumnModel().getColumn(3).setPreferredWidth(300);
-        table.getColumnModel().getColumn(4).setPreferredWidth(100);
-        table.getColumnModel().getColumn(5).setPreferredWidth(300);
+        table.getColumnModel().getColumn(0).setPreferredWidth(25);
+        table.getColumnModel().getColumn(1).setPreferredWidth(30);
+        table.getColumnModel().getColumn(2).setPreferredWidth(200);
+        table.getColumnModel().getColumn(3).setPreferredWidth(542);
+        table.getColumnModel().getColumn(4).setPreferredWidth(300);
+        table.getColumnModel().getColumn(5).setPreferredWidth(100);
+        table.getColumnModel().getColumn(6).setPreferredWidth(300);
         righttable.setHorizontalAlignment(JLabel.RIGHT);
-        table.getColumnModel().getColumn(3).setCellRenderer(righttable);
         table.getColumnModel().getColumn(4).setCellRenderer(righttable);
         table.getColumnModel().getColumn(5).setCellRenderer(righttable);
+        table.getColumnModel().getColumn(6).setCellRenderer(righttable);
 
 //        Tambah ke panel data
         data.add(lno);
         data.add(fno);
         data.add(ltanggal);
+        Timer date = new Timer();
+        date.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                ftanggal.setText(new SimpleDateFormat("E, dd MMMM yyyy HH:mm:ss").format(new java.util.Date()));
+            }
+        },0,1000);
         data.add(ftanggal);
         data.add(lkasir);
         data.add(fkasir);
@@ -123,7 +136,7 @@ public class vUtama extends JFrame{
         jScrollPane.setBounds(10,180,1500,600);
 
         add(tab);
-        tab.setBounds(10,10,1520,825);
+        tab.setBounds(5,0,1530,835);
     }
 
     public static void main(String[] args) {
