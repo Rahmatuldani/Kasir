f6a5ace5124f6446a5b97f80d1a63d74c13c3e25
Rezky Putratama Raharjo <49394196+ekky2412@users.noreply.github.com>
2020-04-03 15:01:40 +0700

    Add tab Barang
    belum selesai

--- src/Utama/vUtama.java
+++ src/Utama/vUtama.java
@@ -4,28 +4,36 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableColumnModel;
 import java.awt.*;
-import java.text.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.*;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.sql.*;
+import java.text.*;
 
 public class vUtama extends JFrame{
     JTabbedPane tab = new JTabbedPane();
     JPanel pKasir = new JPanel();
     JPanel pBarang = new JPanel();
     JPanel pLaporan = new JPanel();
-
+    
+//    Koneksi
+    Connection connection;
+    Statement statement;
+    ResultSet resultSet;
+    
 //    Bagian Kasir
     JLabel lno = new JLabel("No Transaksi : ",SwingConstants.RIGHT);
-    JLabel fno = new JLabel();
     JLabel ltanggal = new JLabel("Tanggal : ",SwingConstants.RIGHT);
-    JLabel ftanggal = new JLabel();
     JLabel lkasir = new JLabel("Kasir : ",SwingConstants.RIGHT);
-    JLabel fkasir = new JLabel();
     JLabel lmember = new JLabel("Member : ",SwingConstants.RIGHT);
     JLabel lkode = new JLabel("Kode Item : ",SwingConstants.RIGHT);
     JLabel ljum = new JLabel("Jumlah : ",SwingConstants.RIGHT);
 
+    JTextField fno = new JTextField(30);
+    JTextField ftanggal = new JTextField(30);
+    JTextField fkasir = new JTextField(30);
     String[] isi = {"UMUM","MEMBER"};
     JComboBox fmember = new JComboBox(isi);
     JTextField fkode = new JTextField(15);
@@ -33,33 +41,37 @@ public class vUtama extends JFrame{
     JTextField ftotal = new JTextField("12.000",30);
 
     JButton bTambah = new JButton("Tambah");
+    JButton bEdit = new JButton("Edit");
     JButton bHapus = new JButton("Hapus");
     JButton bBayar = new JButton("Bayar");
 
     JPanel data = new JPanel();
     JPanel dbarang = new JPanel();
-    String[] column = {"","No","Kode Item","Nama Barang","Harga","Jumlah","Total"};
+    String[] column = {"No","Kode Item","Nama Barang","Harga","Jumlah","Total"};
     int a = 12000;
     int b = 20;
-    Object[][] tdata = {{"","1","123","pensil",a,b,new Integer(a*b)}};
+    Object[][] tdata = {{"1","123","pensil",a,b,new Integer(a*b)}};
     JTable table = new JTable(tdata,column);
     DefaultTableCellRenderer righttable = new DefaultTableCellRenderer();
     JScrollPane jScrollPane = new JScrollPane(table);
 //    End Bagian Kasir
 
+    
+//  Bagian Barang
+    JPanel bHeader = new JPanel();
+    JPanel tBarang = new JPanel();
+    
     public vUtama(){
         setTitle("Sistem Kasir");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setExtendedState(MAXIMIZED_BOTH);
-//        setUndecorated(true);
         setLocationRelativeTo(null);
         setLayout(null);
         setVisible(true);
-
         tab.addTab("Kasir",pKasir);
         tab.addTab("Barang",pBarang);
         tab.addTab("Laporan",pLaporan);
-
+        
 //        Bagian Kasir
         pKasir.setLayout(null);
         dbarang.setLayout(new FlowLayout(FlowLayout.LEADING,30,3));
@@ -70,22 +82,21 @@ public class vUtama extends JFrame{
         ftotal.setFont(new Font("SansSerif",Font.PLAIN,90));
         ftotal.setHorizontalAlignment(SwingConstants.RIGHT);
         ftotal.setEditable(false);
-
+        
 //        Setting Tabel
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         table.setRowHeight(20);
         table.setFont(new Font("SansSerif",Font.PLAIN,14));
-        table.getColumnModel().getColumn(0).setPreferredWidth(25);
-        table.getColumnModel().getColumn(1).setPreferredWidth(30);
-        table.getColumnModel().getColumn(2).setPreferredWidth(200);
-        table.getColumnModel().getColumn(3).setPreferredWidth(542);
-        table.getColumnModel().getColumn(4).setPreferredWidth(300);
-        table.getColumnModel().getColumn(5).setPreferredWidth(100);
-        table.getColumnModel().getColumn(6).setPreferredWidth(300);
+        table.getColumnModel().getColumn(0).setPreferredWidth(30);
+        table.getColumnModel().getColumn(1).setPreferredWidth(200);
+        table.getColumnModel().getColumn(2).setPreferredWidth(567);
+        table.getColumnModel().getColumn(3).setPreferredWidth(300);
+        table.getColumnModel().getColumn(4).setPreferredWidth(100);
+        table.getColumnModel().getColumn(5).setPreferredWidth(300);
         righttable.setHorizontalAlignment(JLabel.RIGHT);
+        table.getColumnModel().getColumn(3).setCellRenderer(righttable);
         table.getColumnModel().getColumn(4).setCellRenderer(righttable);
         table.getColumnModel().getColumn(5).setCellRenderer(righttable);
-        table.getColumnModel().getColumn(6).setCellRenderer(righttable);
 
 //        Tambah ke panel data
         data.add(lno);
@@ -136,8 +147,79 @@ public class vUtama extends JFrame{
         jScrollPane.setBounds(10,180,1500,600);
 
         add(tab);
-        tab.setBounds(5,0,1530,835);
+        tab.setBounds(10,10,1520,825);
+        
+//      End Bagian Kasir
+
+//         Bagian Barang
+        pBarang.setLayout(new BoxLayout(pBarang,BoxLayout.LINE_AXIS));
+        String[] kolom = {"Kode Item","Nama Barang","Jenis Barang","Harga","Stok","Aksi"};
+        String[][] listbarang = new String[readBarang()][6];
+        JTable barangJTable = new JTable(readBarang(listbarang),kolom);
+        JScrollPane jScrollPane = new JScrollPane(barangJTable);
+        
+//        Setting Tabel
+        barangJTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        barangJTable.setRowHeight(20);
+        barangJTable.setFont(new Font("SansSerif",Font.PLAIN,14));
+        barangJTable.getColumnModel().getColumn(0).setPreferredWidth(200);
+        barangJTable.getColumnModel().getColumn(1).setPreferredWidth(567);
+        barangJTable.getColumnModel().getColumn(2).setPreferredWidth(200);
+        barangJTable.getColumnModel().getColumn(3).setPreferredWidth(100);
+        barangJTable.getColumnModel().getColumn(4).setPreferredWidth(100);
+        barangJTable.getColumnModel().getColumn(5).setPreferredWidth(330);
+        righttable.setHorizontalAlignment(JLabel.RIGHT);
+        barangJTable.getColumnModel().getColumn(3).setCellRenderer(righttable);
+        barangJTable.getColumnModel().getColumn(4).setCellRenderer(righttable);
+        barangJTable.getColumnModel().getColumn(5).setCellRenderer(righttable);
+
+//      Memasukkan Data ke dalam Frame        
+        pBarang.add(jScrollPane);
     }
+    
+    public int readBarang(){
+        int i=0;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/kasir", "root", "");
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM barang");
+            while (resultSet.next()){
+                i=i+1;
+            }
+            statement.close();
+            connection.close();
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null,"Database tidak terkoneksi", "Database Error", JOptionPane.ERROR_MESSAGE);
+        }
+        return i;
+    }
+      
+    public String[][] readBarang(String listbarang[][]){
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/kasir", "root", "");
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM barang");
+            int i=0;
+            while (resultSet.next()){
+                listbarang[i][0] = resultSet.getString("id_barang");
+                listbarang[i][1] = resultSet.getString("nama_barang");
+                listbarang[i][2] = resultSet.getString("jenis_barang");
+                listbarang[i][3] = resultSet.getString("harga_barang");
+                listbarang[i][4] = resultSet.getString("stok");
+                listbarang[i][5] = "null";
+                i=i+1;
+            }
+            statement.close();
+            connection.close();
+        } catch (SQLException e){
+            JOptionPane.showMessageDialog(null,"Database tidak terkoneksi", "Database Error", JOptionPane.ERROR_MESSAGE);
+        }
+    return listbarang;
+    }
+    
+    public void crud(){
+    // CRUD BUTTON
+}
 
     public static void main(String[] args) {
         new vUtama();
