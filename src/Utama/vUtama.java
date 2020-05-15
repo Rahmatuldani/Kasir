7da22b36751e341e1dc7b2c9053cf4ceaeb8913f
Rahmatuldani <danirahmatul@gmail.com>
2020-05-15 23:47:29 +0700

    update

--- src/Utama/vUtama.java
+++ src/Utama/vUtama.java
@@ -168,59 +168,59 @@ public class vUtama extends JFrame {
         JScrollPane LaporanjScrollPane = new JScrollPane(laporanJTable);
     }
 
-    public JFrame Tambah(){
-        setTitle("Tambah Barang");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-        setLayout(null);
-        setSize(400,200);
-        add(namaLabel);
-        add(jenisLabel);
-        add(stokLabel);
-        add(hargaLabel);
-        add(nama_barang);
-        add(harga_barang);
-        add(jenis_barang);
-        add(stok_barang);
-        add(tambahBarang);
-        namaLabel.setBounds(20,20,100,20);
-        jenisLabel.setBounds(20,40,100,20);
-        stokLabel.setBounds(20,60,100,20);
-        hargaLabel.setBounds(20,80,100,20);
-        nama_barang.setBounds(150,20,150,20);
-        harga_barang.setBounds(150,40,150,20);
-        jenis_barang.setBounds(150,60,150,20);
-        stok_barang.setBounds(150,80,150,20);
-        tambahBarang.setBounds(100,120,150,20);
-        return currentFrame;
-    }
-
-    public JFrame Edit(){
-        setTitle("Edit Barang");
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
-        setLayout(null);
-        setSize(400,200);
-        add(namaLabel);
-        add(jenisLabel);
-        add(stokLabel);
-        add(hargaLabel);
-        add(nama_barang);
-        add(harga_barang);
-        add(jenis_barang);
-        add(stok_barang);
-        add(editBarang);
-        namaLabel.setBounds(20,20,100,20);
-        jenisLabel.setBounds(20,40,100,20);
-        stokLabel.setBounds(20,60,100,20);
-        hargaLabel.setBounds(20,80,100,20);
-        nama_barang.setBounds(150,20,150,20);
-        harga_barang.setBounds(150,40,150,20);
-        jenis_barang.setBounds(150,60,150,20);
-        stok_barang.setBounds(150,80,150,20);
-        editBarang.setBounds(100,120,150,20);
-        return currentFrame;
-    }
+//    public JFrame Tambah(){
+//        setTitle("Tambah Barang");
+//        setDefaultCloseOperation(EXIT_ON_CLOSE);
+//        setVisible(true);
+//        setLayout(null);
+//        setSize(400,200);
+//        add(namaLabel);
+//        add(jenisLabel);
+//        add(stokLabel);
+//        add(hargaLabel);
+//        add(nama_barang);
+//        add(harga_barang);
+//        add(jenis_barang);
+//        add(stok_barang);
+//        add(tambahBarang);
+//        namaLabel.setBounds(20,20,100,20);
+//        jenisLabel.setBounds(20,40,100,20);
+//        stokLabel.setBounds(20,60,100,20);
+//        hargaLabel.setBounds(20,80,100,20);
+//        nama_barang.setBounds(150,20,150,20);
+//        harga_barang.setBounds(150,40,150,20);
+//        jenis_barang.setBounds(150,60,150,20);
+//        stok_barang.setBounds(150,80,150,20);
+//        tambahBarang.setBounds(100,120,150,20);
+//        return currentFrame;
+//    }
+//
+//    public JFrame Edit(){
+//        setTitle("Edit Barang");
+//        setDefaultCloseOperation(EXIT_ON_CLOSE);
+//        setVisible(true);
+//        setLayout(null);
+//        setSize(400,200);
+//        add(namaLabel);
+//        add(jenisLabel);
+//        add(stokLabel);
+//        add(hargaLabel);
+//        add(nama_barang);
+//        add(harga_barang);
+//        add(jenis_barang);
+//        add(stok_barang);
+//        add(editBarang);
+//        namaLabel.setBounds(20,20,100,20);
+//        jenisLabel.setBounds(20,40,100,20);
+//        stokLabel.setBounds(20,60,100,20);
+//        hargaLabel.setBounds(20,80,100,20);
+//        nama_barang.setBounds(150,20,150,20);
+//        harga_barang.setBounds(150,40,150,20);
+//        jenis_barang.setBounds(150,60,150,20);
+//        stok_barang.setBounds(150,80,150,20);
+//        editBarang.setBounds(100,120,150,20);
+//        return currentFrame;
+//    }
 
     public static void main(String[] args) {
         new MVC();
